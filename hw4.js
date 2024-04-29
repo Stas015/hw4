ede2731fb29afcd5411a8edbd9cf493bfc4999f0
Stas015 <167779562+Stas015@users.noreply.github.com>
2024-04-29 20:11:34 +0200

    Add files via upload

---
+++ hw4.js
@@ -0,0 +1,24 @@
+//1. Ініціалізація числових змінних
+var integerNumber = 4;
+console.log(integerNumber);
+
+var floatingPointNumber = 6.8; 
+console.log(floatingPointNumber);
+
+//2.largeNumber
+var largeNumber = 1e6;
+console.log(largeNumber);
+
+//3. Експоненціальний запис чисел
+var cientificNotation = 1.23e5; // Експоненціальний запис означає 1.23 * 10^5
+console.log(cientificNotation); 
+
+//4. Використання об'єкту Math для генерації випадкових чисел:
+var randomNumber = Math.random(); 
+console.log(randomNumber); 
+
+//5. Особливості чисел у JavaScript:
+const firstDecimal = 0.11;
+const secondDecimal = 0.12;
+const sum = firstDecimal + secondDecimal 
+console.log(sum);  
